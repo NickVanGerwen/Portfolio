29793eeb9a24a2de9f810651293333abcc501de6
NickVanGerwen <nick.gerwen@outlook.com>
2024-10-30 21:24:53 +0100

    update background color and scrollbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,44 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import metalPlateImage from '../Images/about/headshot.jpg';
 import { Link } from 'react-scroll';
 import '../Styling/GameContainer.css';
 import '../Styling/HeaderContainer.css';
 
 function Home() {
-    // Define a function to check if the user has reached the bottom of the page
-    const isBottomOfPage = () => {
-        return window.innerHeight + window.scrollY >= document.documentElement.scrollHeight;
-    };
+    const [flashingTriggered, setFlashingTriggered] = useState(false); // Track if flashing has occurred
 
-    // Define a state variable to track whether the animation has been triggered
-    const [flashingTriggered, setFlashingTriggered] = useState(false);
+    const startColor = { r: 112, g: 144, b: 145 };
+    const endColor = { r: 212, g: 174, b: 121 };
 
-    // Inside your scroll event listener
-    window.addEventListener("scroll", () => {
-        if (!flashingTriggered && isBottomOfPage()) {
-            console.log("Reached the bottom of the page!");
-            // Add the "flash" class to the element with id "bottomElement"
+    // Update scroll thumb color based on scroll position
+    const handleScroll = () => {
+        const scrollTop = window.scrollY;
+        const maxScrollTop = document.documentElement.scrollHeight - window.innerHeight;
+
+        // Calculate scroll percentage
+        const scrollPercent = Math.min(scrollTop / maxScrollTop, 1);
+
+        // Lerp between the start and end colors based on scroll percentage
+        const red = Math.round(startColor.r * (1 - scrollPercent) + endColor.r * scrollPercent);
+        const green = Math.round(startColor.g * (1 - scrollPercent) + endColor.g * scrollPercent);
+        const blue = Math.round(startColor.b * (1 - scrollPercent) + endColor.b * scrollPercent);
+        const newColor = `rgb(${red}, ${green}, ${blue})`;
+
+        // Set the new color to the CSS variable
+        document.documentElement.style.setProperty('--scroll-thumb-color', newColor);
+
+        // Check if bottom of page is reached for flashing effect
+        if (!flashingTriggered && (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight)) {
             const bottomElement = document.getElementById("section5");
             const bottomElement2 = document.getElementById("section5b");
-            if (bottomElement) {
+            if (bottomElement && bottomElement2) {
                 bottomElement.classList.add("flash");
                 bottomElement2.classList.add("flash");
-                // Set the flashingTriggered flag to true
-                setFlashingTriggered(true);
-                // Remove the "flash" class after 1 second (same duration as the animation)
+                setFlashingTriggered(true); // Prevents re-triggering
                 setTimeout(() => {
                     bottomElement.classList.remove("flash");
                     bottomElement2.classList.remove("flash");
                 }, 1000);
             }
         }
-    });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [flashingTriggered]);
 
     document.documentElement.style.overflowX = 'hidden';
     return (
         <>
-            {/* <head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link></head> */}
-
             {/* Nav Bar */}
             <nav id="aboutme" className="navbar navbar-expand-lg navbar-black bg-black fixed-top" style={{ overflowX: 'hidden', paddingLeft: '20px', paddingRight: '20px', width: '100%', opacity: '90%', backgroundColor: 'black' }}>
                 <div className="container-fluid d-flex align-items-center" style={{ position: 'relative' }}>
@@ -71,10 +83,10 @@ function Home() {
             </nav>
 
             {/* About Me */}
-            <div id="section1" style={{ paddingTop: '0', display: 'flex', justifyContent: 'center', width: '100%' }}>
-                <p className="HeaderContainer" style={{ fontWeight: 600, textAlign: 'center', fontSize: '48px', backgroundColor: '#fdae2d', color: 'white', width: '100%', margin: '0', paddingBottom: '20px', paddingTop: '50px' }}>About Me</p>
-            </div>
             <div className='Background' style={{ margin: '0', textAlign: 'center', color: 'black', height: '100%', width: '100vw' }}>
+                <div id="section1" style={{ paddingTop: '0', display: 'flex', justifyContent: 'center', width: '100%' }}>
+                    <p className="HeaderContainer" style={{ fontWeight: 600, textAlign: 'center', fontSize: '48px', color: 'white', width: '100%', margin: '0', paddingBottom: '20px', paddingTop: '50px' }}>About Me</p>
+                </div>
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <div>
                         <img style={{ borderRadius: '5px', margin: '30px', width: '220px', border: '5px solid #211c1c' }} src={metalPlateImage} alt="Headshot" />
@@ -134,14 +146,14 @@ function Home() {
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '25px' }}>
                     {/* Oh Boids! */}
                     <div className="GameContainer" style={{ marginRight: '10px' }}>
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2244129?border_width=5&amp;bg_color=adb9b1&amp;fg_color=222222&amp;link_color=fa5c5c&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/oh-boids">Oh Boids! by Nickvg</a></iframe>
+                        <iframe title="boids" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2244129?border_width=5&amp;bg_color=adb9b1&amp;fg_color=222222&amp;link_color=fa5c5c&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/oh-boids">Oh Boids! by Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >Oh Boids! is a customizable boids simulation. Boids is an example of emergent behaviour which can imitate a flock of birds and in this case a school of fish. </p>
                     </div>
                     <div style={{ marginRight: '20px' }}></div>
 
                     {/* Show Your Metal */}
                     <div className="GameContainer">
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1443875?border_width=5&amp;bg_color=d1e0db&amp;fg_color=222222&amp;link_color=e38b00&amp;border_color=ffffff" width="560" height="175"><a href="https://lokitheshady.itch.io/show-your-metal">Show Your Metal by LokiTheShady, Nickvg</a></iframe>
+                        <iframe title="metal" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1443875?border_width=5&amp;bg_color=d1e0db&amp;fg_color=222222&amp;link_color=e38b00&amp;border_color=ffffff" width="560" height="175"><a href="https://lokitheshady.itch.io/show-your-metal">Show Your Metal by LokiTheShady, Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >This puzzle game was made together with Bart Hagoort. It features a little metal man, making his way through a portal-esque testing facility.</p>
                     </div>
                 </div>
@@ -159,14 +171,14 @@ function Home() {
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '25px' }}>
                     {/* Jans Jam */}
                     <div className="GameContainer" style={{ marginRight: '10px' }}>
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2004414?border_width=5&amp;bg_color=C42F2F&amp;fg_color=222222&amp;link_color=fa5c5c&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/jans-jam">Jans Jam by Nickvg</a></iframe>
+                        <iframe title="jam" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2004414?border_width=5&amp;bg_color=C42F2F&amp;fg_color=222222&amp;link_color=fa5c5c&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/jans-jam">Jans Jam by Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >'Jans Jam' is a short but sweet game about catching as much jam as possible. It is one of my personal favorites because everything, including visuals, sounds and music was made from scratch by me. </p>
                     </div>
                     <div style={{ marginRight: '20px' }}></div>
 
                     {/* Falling Monkey */}
                     <div className="GameContainer">
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1676599?border_width=5&amp;bg_color=ffecd6&amp;fg_color=0d2b45&amp;link_color=ffaa5e&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/falling-monkey">Falling Monkey by Nickvg</a></iframe>
+                        <iframe title="monkey" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1676599?border_width=5&amp;bg_color=ffecd6&amp;fg_color=0d2b45&amp;link_color=ffaa5e&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/falling-monkey">Falling Monkey by Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >A monkey who is done with life, but life is not done with him. 'Falling Monkey' provides a melancholic experience I've yet to recapture. This project increased my respect for sound design with every *boing*.</p>
                     </div>
                 </div>
@@ -174,14 +186,14 @@ function Home() {
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     {/* Blow that Bean off the Table */}
                     <div className="GameContainer" style={{ marginRight: '10px' }}>
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2132730?border_width=5&amp;bg_color=1b1c16&amp;fg_color=2cff2a&amp;link_color=327345&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/blow-that-bean-off-the-table">Blow That Bean Off The Table! by Nickvg</a></iframe>
+                        <iframe title="bean" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2132730?border_width=5&amp;bg_color=1b1c16&amp;fg_color=2cff2a&amp;link_color=327345&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/blow-that-bean-off-the-table">Blow That Bean Off The Table! by Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }}>Inspired by 'The Stanley Parable' and found footage videos like 'The Backrooms', this game served as an exploration of horror through sound and visuals using an unassuming vegetable. Stick around long enough to see the true ending ;). </p>
                     </div>
                     <div style={{ marginRight: '20px' }}></div>
 
                     {/* Extreme Egg Hunt */}
                     <div className="GameContainer">
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1492011?border_width=5&amp;bg_color=222222&amp;fg_color=eeeeee&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/extreme-egg-hunt">EXTREME EGG HUNT by Nickvg</a></iframe>
+                        <iframe title="egg" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/1492011?border_width=5&amp;bg_color=222222&amp;fg_color=eeeeee&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/extreme-egg-hunt">EXTREME EGG HUNT by Nickvg</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >This is the first game I ever submitted to a game jam. Everything was made by in the span of 5 hours and thus, is a good reflection of what my work looks like under high pressure.</p>
                     </div>
                 </div>
@@ -189,7 +201,7 @@ function Home() {
                 {/* BossCade */}
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <div className="GameContainer" >
-                        <iframe style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2514038?border_width=5&amp;bg_color=111111&amp;fg_color=8fd1ff&amp;link_color=7d2953&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/bosscade">BossCade by Nickvg, Inzengo</a></iframe>
+                        <iframe title="boss" style={{ borderRadius: '5px', }} frameBorder="0" src="https://itch.io/embed/2514038?border_width=5&amp;bg_color=111111&amp;fg_color=8fd1ff&amp;link_color=7d2953&amp;border_color=ffffff" width="560" height="175"><a href="https://nickvg.itch.io/bosscade">BossCade by Nickvg, Inzengo</a></iframe>
                         <p style={{ marginTop: '1rem', width: '560px', textAlign: 'left' }} >A fast paced action game where you fight an unnamed yellow arcade boss. It focusses on retro visuals and procedural gameplay that evolves over time.</p>
                     </div>
                 </div>
